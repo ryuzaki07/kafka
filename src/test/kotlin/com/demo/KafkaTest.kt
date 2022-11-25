d6acf03d1c5c69f22afb1a18907e3114906502e8
ryuzaki07 <ryuzaki07>
2022-11-25 10:13:08 +0000

    Initial commit

---
+++ src/test/kotlin/com/demo/KafkaTest.kt
@@ -0,0 +1,19 @@
+package com.demo
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import jakarta.inject.Inject
+
+@MicronautTest
+class KafkaTest {
+
+    @Inject
+    lateinit var application: EmbeddedApplication<*>
+
+    @Test
+    fun testItWorks() {
+        Assertions.assertTrue(application.isRunning)
+    }
+
+}
